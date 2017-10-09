113b26837816fbdf7f7f196aa41386aaccc0dd73
Jordan Thaler <jordanpthaler@gmail.com>
2017-10-09 16:43:11 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'fvY5JGEOFsDh6nhFZNy3C4eFG',
+  consumer_secret: '7YJJ4NzMKMgC7wFIJg2lUnYSVef26UBSqvVo4TymXdEO6YgFh1',
+  access_token_key: '916102457209573376-hm00el4PSIOpRxoaBjSEOCKV27XwQoE',
+  access_token_secret: 'iloLrlzvSzt72qBxbGMZ2x2VNjSHdcIu57VUI8PlM7qpb',
+}
+
+module.exports = twitterKeys;
+
+var spotifyKeys = {
+	id: "69381049e480491b9fa690acaee8c56d"
+	secret: "6d8621ddee254ff3898747a954997789"
+}
